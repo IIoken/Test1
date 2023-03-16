7b097397677a9aee98b1b3662c0aacb0eea0b4cb
IIoken <iioken@yandex.ru>
2023-03-16 20:25:11 +0400

    Probnoe

---
+++ Animal/src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public void main(String[] args) {
+        Cat cat  = new Cat();
+        cat.sound();
+    }
+    public class Animal {
+
+        private boolean vegetarian;
+        private String eats;
+
+        public Animal(){}
+
+        public Animal(boolean veg, String food){
+            this.vegetarian = veg;
+            this.eats = food;
+        }
+
+        public void sound(){
+        }
+
+        public boolean isVegetarian() {
+            return vegetarian;
+        }
+
+        public String getEats() {
+            return eats;
+        }
+    }
+
+    class Cat extends Animal{
+        public void sound(){
+            System.out.println("Meow");
+        }
+    }
+}
