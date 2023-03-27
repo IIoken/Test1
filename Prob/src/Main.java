460a1637b03edb454150da08412a3b22a995dd97
IIoken <iioken@yandex.ru>
2023-03-27 12:23:22 +0400

    Просто решил обновить

--- Prob/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Squirrel s = new Squirrel(new int[] {5, 5, -3, -1});
-        System.out.println(s.maxNuts());
-        Squirrel s2 = new Squirrel(new int[] {1, -2, 3, 1, -1, -4, 2, 3});
-        System.out.println(s2.maxNuts());
-    }
-}
