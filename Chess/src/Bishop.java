85334c39170ba9fad8ba29386d229c36734b3190
IIoken <iioken@yandex.ru>
2023-11-17 14:10:52 +0400

    Save2

--- Chess/src/Bishop.java
+++ Chess/src/Bishop.java
@@ -12,7 +12,10 @@ public class Bishop extends ChessPiece {
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
         if (line != toLine && column != toColumn) {
             if (toLine >= 0 && toLine <= 7 && toColumn >= 0 && toColumn <= 7) {
-                if ((toLine - line == toColumn - column)) {
+//                Сделали линию по вертикали
+                if (((toLine - line) == (toColumn - column)
+//                        ПО горизонтали
+                        || (toLine - line) == -1*(toColumn - column))) {
                     return true;
                 } else return false;
             } else return false;
