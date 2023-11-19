3112de90690d06c9833af288a14136fdc4d90505
IIoken <iioken@yandex.ru>
2023-11-19 16:11:27 +0400

    Save7

--- Chess/src/Bishop.java
+++ Chess/src/Bishop.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Bishop extends ChessPiece {
     public Bishop(String color) {
         super(color);
@@ -12,21 +14,41 @@ public class Bishop extends ChessPiece {
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
         if (line != toLine && column != toColumn) {
             if (toLine >= 0 && toLine <= 7 && toColumn >= 0 && toColumn <= 7) {
-//                Сделали линию по вертикали
+//                Движение по горизонтали в верх
                 if ((toLine - line) == (toColumn - column) && isHodBishop(chessBoard, line, column, toLine, toColumn)
-//                        ПО горизонтали
-                        || (toLine - line) == -1*(toColumn - column) && isHodBishop(chessBoard, line, column, toLine, toColumn)) {
+//                        в низ
+                        || (toLine - line) == -1 * (toColumn - column) && isHodBishop(chessBoard, line, column, toLine, toColumn)) {
                     return true;
                 } else return false;
             } else return false;
         } else return false;
     }
-    private boolean isHodBishop(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-        if (chessBoard.board[toLine][toColumn] == null || chessBoard.board[line][column].getColor() != chessBoard.board[toLine][toColumn].getColor()){
-            return true;
-        }return false;
-    }
 
+    private boolean isHodBishop(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        int l = 0;
+        int c = 0;
+        for (int i = 1; i < Math.abs(toLine - line); i = i + 1) {
+            if (toLine > line) {
+                l = line + 1;  // Движение в ввехр
+                if (toColumn > column) {
+                    c = column + 1;
+                } else c = column - 1;
+            } else {
+                l = line - 1; // Движение в низ
+                if (toColumn > column) {
+                    c = column + 1;
+                } else c = column - 1;
+            }}
+            if (chessBoard.board[l][c] == null) { // Проверка что клетка пуста
+                return true;
+            } else if (l == toLine && c == toColumn && !Objects.equals(chessBoard.board[toLine][toColumn].getColor(), chessBoard.board[line][column].getColor())) {
+                return true;
+            } else return false;
+        }
+
+
+
+    //    chessBoard.board[toLine][toColumn] == null || chessBoard.board[line][column].getColor() != chessBoard.board[toLine][toColumn].getColor()
     @Override
     public String getSymbol() {
         return "B";
--- Chess/src/Rook.java
+++ Chess/src/Rook.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Rook extends ChessPiece {
     public Rook(String color) {
         super(color);
@@ -10,13 +12,47 @@ public class Rook extends ChessPiece {
 
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-//        if (line != toLine && column != toColumn) {
-            if (toLine >= 0 && toLine <= 7 && toColumn >= 0 && toColumn <= 7) {
-                if (((toLine != line) && (toColumn == column)) || ((toLine == line) && (toColumn != column))) {
-                    return true;
-                } else return false;
+        if (toLine >= 0 && toLine <= 7 && toColumn >= 0 && toColumn <= 7) {
+            if (((toLine != line) && (toColumn == column) && isHodLine(chessBoard, line, toLine, column, toColumn))
+                    || ((toLine == line) && (toColumn != column) && isHodColumn(chessBoard, line, toLine, column, toColumn))) {
+                return true;
             } else return false;
-//        } else return false;
+        } else return false;
+    }
+
+    private boolean isHodLine(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        int l = 0;
+
+        for (int i = 1; i < Math.abs(toLine - line); i++) {
+            if (toLine > line) {
+                l = line + 1;
+            } else l = line - 1;
+        }
+        if (chessBoard.board[l][column] == null) {
+            return true;
+        } else {
+            if (l == toLine && (chessBoard.board[line][column].getColor() != chessBoard.board[toLine][toColumn].getColor())) {
+                return true;
+            }else return false;
+        }
+        }
+
+    private boolean isHodColumn(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        int c = 0;
+
+        for (int i = 1; i < Math.abs(toColumn - column); i++) {
+            if (toColumn > column) {
+                c = column + 1;
+            } else c = column - 1;
+        }
+        if (chessBoard.board[line][c] == null) {
+            return true;
+        } else {
+           if (c == toColumn && (chessBoard.board[line][column].getColor() != chessBoard.board[toLine][toColumn].getColor())){
+                return true;
+            }else return false;
+
+        }
     }
 
     @Override
