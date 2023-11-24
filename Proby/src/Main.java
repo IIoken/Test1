0bfaed9b63612a1c11afebf2bf7e61ebcb4d1e77
IIoken <iioken@yandex.ru>
2023-11-24 09:32:55 +0400

    Save10

--- Proby/src/Main.java
+++ Proby/src/Main.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.lang.*;
 public class Main{
   public static void main(String[] args) {
-          
+    Date date = new Date();
+    System.out.println(date);
   }
 }
